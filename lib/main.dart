431c3adc9600d498f311cb454d3b5e592e0609d6
Gustavo Andrade <guustavo.andrade04@gmail.com>
2026-05-13 18:43:59 -0300

    feat(env): refactor main to use environment variable for bootstrap configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:devlingo/src/app/bootstrap.dart';
 
+const _environment = String.fromEnvironment(
+  'ENVIRONMENT',
+  defaultValue: 'dev',
+);
+
 Future<void> main() async {
-  await bootstrap(environment: 'dev');
+  await bootstrap(environment: _environment);
 }

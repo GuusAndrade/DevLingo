1b661669d302675aa4b137081bc8a1ce176b1f99
Gustavo Andrade <guustavo.andrade04@gmail.com>
2026-05-13 18:44:18 -0300

    feat(env): add lazy singleton for staging environment in dependency injection module

--- lib/src/core/di/register_module.dart
+++ lib/src/core/di/register_module.dart
@@ -2,6 +2,7 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:devlingo/src/app/environment/env.dart';
 import 'package:devlingo/src/app/environment/env_dev.dart';
 import 'package:devlingo/src/app/environment/env_prod.dart';
+import 'package:devlingo/src/app/environment/env_staging.dart';
 import 'package:injectable/injectable.dart';
 
 /// Provides third-party dependencies that the app does not own, plus the
@@ -14,6 +15,10 @@ abstract class RegisterModule {
   @dev
   @lazySingleton
   Env get envDev => EnvDev();
+
+  @Environment('staging')
+  @lazySingleton
+  Env get envStaging => EnvStaging();
 
   @prod
   @lazySingleton

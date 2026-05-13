a598f9c596247c4d7836548e3d755a79069f97c7
Gustavo Andrade <guustavo.andrade04@gmail.com>
2026-05-13 18:18:39 -0300

    refactor(main): update bootstrap function to use string environment parameter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:devlingo/src/app/bootstrap.dart';
-import 'package:devlingo/src/app/environment/env_dev.dart';
 
 Future<void> main() async {
-  await bootstrap(env: EnvDev());
+  await bootstrap(environment: 'dev');
 }

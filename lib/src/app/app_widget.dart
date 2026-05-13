040196edafb4836443b35de186d9cfd252504a5f
Gustavo Andrade <guustavo.andrade04@gmail.com>
2026-05-13 19:33:51 -0300

    feat(app): add dark theme to AppWidget

--- lib/src/app/app_widget.dart
+++ lib/src/app/app_widget.dart
@@ -1,6 +1,7 @@
 import 'package:devlingo/src/app/environment/env.dart';
 import 'package:devlingo/src/core/di/injection.dart';
 import 'package:devlingo/src/core/routing/app_router.dart';
+import 'package:devlingo/src/design_system/design_system.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
@@ -11,6 +12,7 @@ class AppWidget extends StatelessWidget {
     return MaterialApp.router(
       title: getIt<Env>().appName,
       debugShowCheckedModeBanner: false,
+      theme: AppTheme.dark,
       routerConfig: getIt<AppRouter>().config,
     );
   }
